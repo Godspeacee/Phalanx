6f21a53e348fbc814ccc846accc05bf4a22acd69
Godspeace Adetayo <adetayogodspeace2003@gmail.com>
2025-07-05 00:33:30 +0100

    Implement Pagination

--- app/issues/components/Pagination.tsx
+++ app/issues/components/Pagination.tsx
@@ -1,9 +1,11 @@
+"use client";
 import { Button, Flex, Text } from "@radix-ui/themes";
 import React from "react";
 import DoubleArrowLeftIcon from "./DoubleArrowLeftIcon";
 import DoubleArrowRightIcon from "./DoubleArrowRightIcon";
 import ChevronRightIcon from "./ChevronRightIcon";
 import ChevronLeftIcon from "./ChevronLeftIcon";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface Props {
   itemCount: number;
@@ -12,23 +14,40 @@ interface Props {
 }
 
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const pageCount = Math.ceil(itemCount / pageSize);
   if (pageCount <= 1) return null;
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push(`?${params.toString()}`);
+  };
+
   return (
     <Flex align={"center"} gap={"2"}>
       <Text size={"2"}>
         Page{currentPage} of {pageCount}
       </Text>
-      <Button disabled={currentPage === 1}>
+      <Button disabled={currentPage === 1} onClick={() => changePage(1)}>
         <DoubleArrowLeftIcon />
       </Button>
-      <Button disabled={currentPage === 1}>
+      <Button
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         <ChevronLeftIcon />
       </Button>
-      <Button disabled={currentPage === pageCount}>
+      <Button
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <ChevronRightIcon />
       </Button>
-      <Button disabled={currentPage === pageCount}>
+      <Button
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+      >
         <DoubleArrowRightIcon />
       </Button>
     </Flex>
