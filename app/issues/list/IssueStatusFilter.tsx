67e010e02f940f885ebdbfc770db0503cf980fe5
Godspeace Adetayo <adetayogodspeace2003@gmail.com>
2025-07-05 01:49:31 +0100

    Build the issueSummary component

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,12 +1,21 @@
 "use client";
 import { Status } from "@/app/generated/prisma";
 import { Select } from "@radix-ui/themes";
+import Link from "next/link";
 
 import { useRouter } from "next/navigation";
 
 const statuses: { label: string; value: Status; key: string }[] = [
-  { label: "Open", value: "OPEN", key: "open" },
-  { label: "In progress", value: "IN_PROGRESS", key: "in_progress" },
+  {
+    label: "Open",
+    value: "OPEN",
+    key: "open",
+  },
+  {
+    label: "In progress",
+    value: "IN_PROGRESS",
+    key: "in_progress",
+  },
   { label: "Closed", value: "CLOSED", key: "closed" },
 ];
 
@@ -14,21 +23,23 @@ const IssueStatusFilter = () => {
   const router = useRouter();
 
   return (
-    <Select.Root
-      onValueChange={(status) => {
-        const query = status ? `?status=${status}` : "";
-        router.push("/issues/list" + query);
-      }}
-    >
-      <Select.Trigger placeholder="Filter by status..." />
-      <Select.Content>
-        {statuses.map((status) => (
-          <Select.Item key={status.key} value={status.value}>
-            {status.label}
-          </Select.Item>
-        ))}
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        onValueChange={(status) => {
+          const query = status ? `?status=${status}` : "";
+          router.push("/issues/list" + query);
+        }}
+      >
+        <Select.Trigger placeholder="Filter by status..." />
+        <Select.Content>
+          {statuses.map((status) => (
+            <Select.Item key={status.key} value={status.value}>
+              {status.label}
+            </Select.Item>
+          ))}
+        </Select.Content>
+      </Select.Root>
+    </>
   );
 };
 
